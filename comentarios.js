559e1ee2a12f6aaea1fb5292b8c21fd96b282097
GiovannaMadruga <giovannamadruga15@gmail.com>
2026-02-26 12:13:05 -0300

    Inicio com JS

---
+++ comentarios.js
@@ -0,0 +1,13 @@
+// o console é utilizado para imprimir algo na tela
+
+console.log("sesi");
+//console.log("senai");
+
+/* 
+
+console.log("senai");
+console.log("senai");
+console.log("senai");
+console.log("senai");
+
+*/
---
+++ desafio_blocos.js
@@ -0,0 +1,14 @@
+/*
+  Desafio: criar dois blocos de código.
+  Onde o primeiro deve impremir na tela os
+  números pares de 0 a 10 e o segundo
+  bloco deve imprimir na tela os impares.
+*/
+
+{
+console.log("Numeros pares: 0, 2, 4, 6, 8 e 10" + "\n");
+}
+
+{
+    console.log("Numeros impares: 1, 3, 5, 7 e 9")
+}
