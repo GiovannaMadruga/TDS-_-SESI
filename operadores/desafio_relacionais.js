a325cd595ed4c2c3177d22fc669952806785366f
GiovannaMadruga <giovannamadruga15@gmail.com>
2026-03-06 11:39:23 -0300

    Operadores de Atribuição e ralacionais

---
+++ operadores/desafio_relacionais.js
@@ -0,0 +1,18 @@
+let a = 10;
+let b = 11;
+
+// impremir no console 5 números verdadeiros(true)
+console.log(b > a);
+console.log(a < b);
+console.log(b >= a);
+console.log(a <= b);
+console.log(a != b);
+
+console.log("\n"); // deixar bonito
+
+// Imprimir no console  números falsos(false)
+console.log(b < a);
+console.log(b < a);
+console.log(a >= b);
+console.log(b <= a);
+console.log(a == b);
