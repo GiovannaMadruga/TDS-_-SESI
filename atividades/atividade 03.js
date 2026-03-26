032e141879dd50c946733c6e381c900f05d84a42
GiovannaMadruga <giovannamadruga15@gmail.com>
2026-03-26 12:12:30 -0300

    Atividades

---
+++ atividades/atividade 03.js
@@ -0,0 +1,9 @@
+// Desafio: Validador de Acesso
+
+var temIdade = 18
+var acessoVIP = true;
+
+if(temIdade >= 18 && acessoVIP){
+let resultado =" #AND Você pode entrar no eveto VIP"
+console.log(resultadoAND);
+}
