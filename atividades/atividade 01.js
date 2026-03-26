e411f6691aebf5c5a60edd5b7d70027641f449f0
GiovannaMadruga <giovannamadruga15@gmail.com>
2026-03-26 11:58:14 -0300

    Atividades

---
+++ atividades/atividade 01.js
@@ -0,0 +1,14 @@
+// Desafio: cadastro de produtos
+
+var produto1 = "Notebook da Apple";
+var preco1 = 8500;
+var preferencia = true;
+
+console.log(`O item é um ${produto1} e custa R$ ${preco1}`);
+
+if (preferencia = true) {
+    console.log("Disponibilidade: true");
+
+} else {
+    console.log("Disponibilidade: false")
+}
---
+++ atividades/atividade 02.js
@@ -0,0 +1,6 @@
+// Desafio: calculadora idade canina
+
+var idadeCachorro = 8;
+let resultado = idadeCachorro * 7;
+
+console.log(`O cachorro tem ${resultado} anos, se ele fosse um humano`);
