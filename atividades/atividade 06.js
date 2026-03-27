2b9b5deb03ed35b03c0b6a391bf15fb92db2eb16
GiovannaMadruga <giovannamadruga15@gmail.com>
2026-03-27 12:03:42 -0300

    Atividades

---
+++ atividades/atividade 06.js
@@ -0,0 +1,14 @@
+var nota1 = 7;
+var nota2 = 10;
+var nota3 = 5;
+var media = (nota1 + nota2 + nota3) / 3;
+
+if(media >= 7) {
+    console.log("Está aprovado!");
+
+} else if (media >= 5 && media <= 6.9){
+    console.log("Está em recuperação, estude!")
+
+    } else{ (media <5) 
+    console.log("Está reprovado, acabou tudo!")
+}
