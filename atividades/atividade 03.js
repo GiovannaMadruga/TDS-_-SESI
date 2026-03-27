a076a3b0f0154353e8f7ab2fcf944851275a920e
GiovannaMadruga <giovannamadruga15@gmail.com>
2026-03-27 10:43:33 -0300

    Atividades

--- atividades/atividade 03.js
+++ atividades/atividade 03.js
@@ -1,9 +1,13 @@
 // Desafio: Validador de Acesso
 
-var temIdade = 18
+var temIdade = 14
 var acessoVIP = true;
 
 if(temIdade >= 18 && acessoVIP){
-let resultado =" #AND Você pode entrar no eveto VIP"
-console.log(resultadoAND);
+    let resultadoAND = "Você pode entrar no eveto VIP = ";
+    resultadoAND += temIdade && acessoVIP ;
+    console.log(resultadoAND);
+} else {
+    let resultadoAND = "Você não pode entrar, é menor de 18!"
+    console.log(resultadoAND);
 }
